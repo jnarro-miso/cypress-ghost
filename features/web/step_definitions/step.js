27133b78c0bcabd5bc48c02d665368e503a56251
Leonel Fonseca <nelsonleonel.fonseca@bbva.com>
2024-05-07 15:45:09 -0500

    Agregar escenario kraken para verificar ceacion de tags

--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -208,3 +208,53 @@ Then('I validate that the member was edited succesful', async function () {
   } 
   return;
 });
+
+When('I click tags', async function() {
+  let element = await this.driver.$('#ember29');
+  return await element.click();
+})
+
+When('I click newTag', async function() {
+  let elements = await this.driver.$$('//a[contains(@class, "gh-btn-primary") and span[text()="New tag"]]'); // Usando $$ para obtener una lista de elementos
+  if (elements.length > 0) {
+      await elements[0].click();
+  } else {
+      throw new Error('Element not found');
+  }
+});
+
+When('I enter tagName {kraken-string}', async function (tagName) {
+  let element = await this.driver.$('#tag-name');
+  return await element.setValue(tagName);
+});
+
+When('I enter tagColor {kraken-string}', async function (tagColor) {
+  let element = await this.driver.$('[name="accent-color"]');
+  return await element.setValue(tagColor);
+});
+
+When('I enter tagSlug {kraken-string}', async function (tagSlug) {
+  let element = await this.driver.$('#tag-slug');
+  return await element.setValue(tagSlug);
+});
+
+When('I enter tagDescription {kraken-string}', async function (tagDescription) {
+  let element = await this.driver.$('#tag-description');
+  return await element.setValue(tagDescription);
+});
+
+When('I click save', async function() {
+  let element = await this.driver.$('[data-test-button="save"]');
+  return await element.click();
+});
+
+Then('I should see the tag {kraken-string}', async function(tagName) {
+  
+  let element = await this.driver.$('h3.gh-tag-list-name[data-test-tag-name=""]');
+
+   // Verifica si el texto del elemento coincide con el texto esperado
+   let texto = await element.getText();
+   if (texto !== tagName) {
+      throw new Error(`Expected title "${tagName}" but found "${texto}"`);
+   }
+});
--- scenariosKraken/EP20.feature
+++
@@ -1,9 +0,0 @@
-Feature: Semana 5
-
-@user1 @web
-Scenario: EP10- Editar descripción de portal principal
-  Given I navigate to page "<GHOST_ADMIN_URL>"
-  When I enter username "<USERNAME>"
-  And I enter password "<PASSWORD>"
-  And I click Sign in
-  Then I access to the view site
